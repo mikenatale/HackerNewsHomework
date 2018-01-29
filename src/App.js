d3a2eb7a371cdb3c4971be0d38f6703431578118
Mike Natale <mnatale@streamsystems.ca>
2018-01-29 11:43:49 -0500

    Get all Commenters on the top stories
    ...and their comment count.

--- src/App.js
+++ src/App.js
@@ -10,12 +10,16 @@ class App extends Component {
         super();
 
         this.state = {
+            commentIds: [],
+            commenters: [],
             topArticlesIds: [],
-            topArticlesInfo: []
+            topArticlesInfo: [],
+            topCommenterIds: []
         };
 
         this.getTopArticles = this.getTopArticles.bind(this);
         this.getTopArticlesInfo = this.getTopArticlesInfo.bind(this);
+        this.getCommenters = this.getCommenters.bind(this);
     }
 
     getTopArticles() {
@@ -41,14 +45,14 @@ class App extends Component {
         let scope = this;
 
         return new Promise((resolve, reject) => {
-            let articlePromises = [];
+            let articleInfoPromises = [];
             this.state.topArticlesIds.forEach((id) => {
-                articlePromises.push(
+                articleInfoPromises.push(
                     axios.get(`${getBaseApiUrl()}/item/${id}.json`)
                 )
             });
 
-            axios.all(articlePromises)
+            axios.all(articleInfoPromises)
             .then((response) => {
                 let topArticlesInfo = response.map(r => r.data);
 
@@ -64,12 +68,63 @@ class App extends Component {
         });
     }
 
+    getCommenters() {
+        let scope = this;
+
+        return new Promise((resolve, reject) => {
+            let commentIds = [];
+            let commentInfoPromises = [];
+            let commenters = [];
+
+            this.state.topArticlesInfo.forEach((article) => {
+                if (article.kids) {
+                    article.kids.forEach((commentId) => {
+                        commentIds.push(commentId);
+                    });
+                }
+            });
+
+            commentIds.forEach((id) => {
+                commentInfoPromises.push(
+                    axios.get(`${getBaseApiUrl()}/item/${id}.json`)
+                )
+            });
+
+            axios.all(commentInfoPromises)
+            .then((comments) => {
+                comments.forEach((comment) => {
+                    if (comment.data.by) {
+                        if (!commenters[comment.data.by]) {
+                            commenters[comment.data.by] = 0;
+                        }
+
+                        commenters[comment.data.by] += 1;
+                    }
+                });
+
+                scope.setState(
+                    {
+                        commentIds,
+                        commenters
+                    },
+                    () => { resolve(); }
+                )
+            })
+            .catch((e) => {
+                console.error(e);
+                reject();
+            });
+        });
+    }
+
     componentWillMount() {
         let scope = this;
 
         scope.getTopArticles().then(() => {
             scope.getTopArticlesInfo().then(() => {
-                // TODO
+                scope.getCommenters().then(() => {
+                    // TODO
+                });
             });
         });
     }
@@ -77,8 +132,7 @@ class App extends Component {
     render() {
         return (
             <div>
-                { JSON.stringify(this.state.topArticlesIds) }
-                { JSON.stringify(this.state.topArticlesInfo) }
+                { JSON.stringify(this.state.commentIds) }
             </div>
         );
     }
